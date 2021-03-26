816981e999964d4d0ca1cfb7ad6991f07cf56cb3
Chimi6 <43803253+Chimi6@users.noreply.github.com>
2021-03-26 07:50:19 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ curl -v -X POST http://20.185.176.246:4243/containers/gradel-sample/kill
 curl -v -X DELETE http://20.185.176.246:4243/containers/gradel-sample?force=true
 curl -v -X POST http://20.185.176.246:4243/images/prune?dangling=true
 sleep 5
-curl -v -X POST http://20.185.176.246:4243/images/create?fromImage=calebcall97/gradelsample:latest-master
+curl -v -X POST http://20.185.176.246:4243/images/create?fromImage=calebcall97/gradelsample:latest
 curl -v -X POST -H "Content-Type: application/json" -d "@docker.json" http://20.185.176.246:4243/containers/create?name=gradel-sample
 sleep 5
 curl -v -X POST http://20.185.176.246:4243/containers/gradel-sample/start
